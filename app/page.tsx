9edff58168fb8cec1902d14d3e1df8089ee21701
psyfer <psyferblock.io@gmail.com>
2023-08-27 17:39:26 +0300

    prettier and prisma config

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="w-screen">
-     <div className=" flex justify-items-end m-3">
-       <div > logo</div>
-       <ul className="flex justify-between">
-        <li><Link href="#">  about</Link></li>
-        <li><Link href="#">  our offering </Link></li>
-        <li><Link href="#">  set up my busing </Link></li>  
-
-        
-       </ul>
-       <div><button className="rounded-lg bg-yellow-500">contact us </button></div>
-     </div>
+      <div className=" flex justify-items-end m-3">
+        <div> logo</div>
+        <ul className="flex justify-between">
+          <li>
+            <Link href="#"> about</Link>
+          </li>
+          <li>
+            <Link href="#"> our offering </Link>
+          </li>
+          <li>
+            <Link href="#"> set up my busing </Link>
+          </li>
+        </ul>
+        <div>
+          <button className="rounded-lg bg-yellow-500">contact us </button>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
