e45e002e6b07ba64f1e33d2653fbe6e1c1b270cc
psyfer <psyferblock.io@gmail.com>
2023-08-27 17:27:12 +0300

    prettier issues

---
+++ app/add-product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddProductPage() {
+  return (
+    <div>
+        <h1 className='text-lg mb-3 font-bold '></h1>
+    </div>
+  )
+}
+
+export default AddProductPage
