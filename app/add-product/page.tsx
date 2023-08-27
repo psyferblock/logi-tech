6a03ea95b7074efd25e6a90cac066be025f8f3a8
psyfer <psyferblock.io@gmail.com>
2023-08-27 17:51:48 +0300

    removed auth for de[ployment

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 function AddProductPage() {
   return (
     <div>
-        <h1 className='text-lg mb-3 font-bold '></h1>
+        <h1 className='text-lg mb-3 font-bold '>Add Product</h1>
+        <input type="text"
+        name=''
+        placeholder=''
+        className=' input input-bordered' />
     </div>
   )
 }
