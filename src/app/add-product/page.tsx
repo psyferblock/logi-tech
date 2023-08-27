d404bc4fe581be3a22fb93a094b861f06b9ae772
psyfer <psyferblock.io@gmail.com>
2023-08-27 19:12:17 +0300

    server action components test

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,10 +1,37 @@
+import { prisma } from "@/src/db/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
+export const metadata = {
+  title: "Add Product Logi-Tech",
+};
+
+// add product function
+async function addProduct(formData: FormData) {
+  "use server";
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  console.log("formData", formData.keys());
+  console.log('formData.values(', formData.values())
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("missing required credentials");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+  redirect("/");
+
+}
 function AddProductPage() {
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold ">Add Product</h1>
-      <form action="">
+      <form action={addProduct}>
         <input
           required
           name="name"
