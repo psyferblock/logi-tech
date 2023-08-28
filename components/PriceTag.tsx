4124ea03067aee5ab86d69d1bd508e022cf91486
psyfer <psyferblock.io@gmail.com>
2023-08-28 14:37:08 +0300

    pricetag component

---
+++ components/PriceTag.tsx
@@ -0,0 +1,17 @@
+import { formatPrice } from '@/lib/format'
+import React from 'react'
+
+interface PriceTagPropsInterface {
+    price:number,
+    className:string
+}
+function PriceTag({price,className}:PriceTagPropsInterface) {
+
+  return (  
+    <div>
+        <span className={`badge ${className}`}>{formatPrice(price)}</span>
+    </div>
+  )
+}
+
+export default PriceTag
