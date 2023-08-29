84a01cd92a5598bd48d9e4273115978e1eaaf610
psyfer <psyferblock.io@gmail.com>
2023-08-29 11:49:25 +0300

    loading page with daisy ui

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <span className="loading loading-dots loading-lg m-auto block">
+        
+    </span>
+  )
+}
+
+export default LoadingPage
