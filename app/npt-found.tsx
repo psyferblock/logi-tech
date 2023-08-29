0b5168d377b5f1c34ddf1068b8d65ac64dfe52ca
psyfer <psyferblock.io@gmail.com>
2023-08-29 09:05:01 +0300

    basic not found page

---
+++ app/npt-found.tsx
@@ -0,0 +1,5 @@
+// this is a special file for nextjs to notice so the name is important
+
+export default function NotFoundPage() {
+  return <div>page not found </div>;
+}
--- app/product/[id]/pages.tsx
+++ app/product/[id]/pages.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
 
-function pages() {
-  return (
-    <div>pages</div>
-  )
-}
 
-export default pages
+interface ProductPagePropsInterface {
+    params: {
+      id: string;
+    };
+  }
+import { prisma } from '@/lib/db/prisma';
+import { notFound } from 'next/navigation';
+  import React from 'react'
+  
+  async function ProductPage(
+    {params:{id}}:ProductPagePropsInterface
+  ) {
+
+    const product= await prisma.product.findUnique({where:{id}})
+    // this redirects us to the not found page 
+    if(!product) notFound()
+
+    return (
+      <div>pages</div>
+    )
+  }
+  
+  export default ProductPage
