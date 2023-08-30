b9d3ee37c0ba7dfad254c27208b315bf305e1795
psyfer <psyferblock.io@gmail.com>
2023-08-30 19:21:33 +0300

    actions types fix

--- app/cart/actions.ts
+++ app/cart/actions.ts
@@ -1,13 +1,13 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { createCart, getCart } from "./cart";
-import { prisma } from "./prisma";
+import { createCart, getCart } from "../../lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
 
 // this function is to set a property and to revalidate path.
 
 export async function setProductQuantity(productId: string, quantity: number) {
-  const cart = (await getCart()) && (await createCart());
+  const cart = (await getCart()) ?? (await createCart());
 
   const articleInCart = cart?.items.find(
     (item) => item.productId === productId
@@ -30,7 +30,7 @@ export async function setProductQuantity(productId: string, quantity: number) {
     } else {
       await prisma.cartItem.create({
         data: {
-          cartId: cart?.id,
+          cartId: cart.id,
           productId,
           quantity,
         },
